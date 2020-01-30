e376d88dc27112897b47e7d85e9927b12317f99d
ochornolutskyi <ochornolutskyi@gmail.com>
2020-01-30 14:16:16 +0200

    pre-release v0.5

--- app.js
+++ app.js
@@ -6,20 +6,25 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public/'));
-app.use(express.static(__dirname + '/public/views/'));
-app.use(express.static(__dirname + '/public/views/partials/'));
+// app.use(express.static(__dirname + '/views/'));
+// app.use(express.static(__dirname + '/public/views/partials/'));
 
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('index');
+    res.render('index',{
+        title: 'СVConstructor'
+    });
 });
 app.get('/view', (req, res) => {
-    res.send('viewCV');
+    res.render('partials/viewcv',{
+        title: 'Author CV'
+    });
 });
-app.get('/auth', (req, res) => {
-    res.end("<h1>Login or register curriculum vitae </h1>");
-});
-app.get('/create', (req, res) => {
-    res.end('<h1> Create your curriculum vitae </h1>');
-});
+// app.get('/auth', (req, res) => {
+//     res.end("<h1>Login or register curriculum vitae </h1>");
+// });
+// app.get('/create', (req, res) => {
+//     res.end('<h1> Create your curriculum vitae </h1>');
+// });
 app.listen(PORT, () => console.log(`We work on ${PORT} port`));
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,91 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     //change color of title
     let i = 0,
-    colors = ['E4A834', '68E3AD'];
+        colors = ["EDE04B", "68E3AD"];
     function nextColor() {
-        document.getElementById('headerTitle').style.color = '#' + colors[i++];
-        if (i >= colors.length) i = 0;
+        if (document.getElementById("headerTitle") == undefined) {
+            return false;
+        } else {
+            document.getElementById("headerTitle").style.color =
+                "#" + colors[i++];
+            if (i >= colors.length) i = 0;
+        }
     }
-    document.getElementById('headerTitle').style.transition = '5s ease';
     setInterval(nextColor, 5000);
+    //login pulse
+    function animationLogin() {
+        let login = document.getElementById("auth");
+        if (login == undefined) {
+            return false;
+        } else
+            login.addEventListener("click", function(event) {
+                event.preventDefault();
+            });
+    }
+    animationLogin();
+
+    //view CV
+    // document
+    //     .getElementById("viewCV")
+    //     .addEventListener("click", function(event) {
+    //         event.preventDefault();
+    //     });
+    //FIXME
+    function addError() {
+        let divError = document.createElement("div");
+        divError.id = "divError";
+        divError.innerHTML =
+            "Sorry, this section is in development. Automatically close after 3s";
+        divError.style = `max-width: 600px;
+        width: 100%;
+        min-height: 45px;
+        padding: 10px;
+        font-size: 20px;
+        background-color: rgba(154,190,170, 0.9);
+        box-shadow: 0 0 25px rgba(154,190,170, 0.9);
+        position: fixed;
+        line-height: 45px;
+        border-radius: 6px;
+        top: 40%;
+        left: 50%;
+        margin-left: -23%;
+        text-align: center;
+        d`;
+        document.body.append(divError);
+    }
+    function closeError() {
+        return document.getElementById("divError").remove();
+    }
+    let lockLink = element => {
+        return (element.style.pointerEvents = "none");
+    };
+    let unlockLink = element => {
+        return (element.style.pointerEvents = "auto");
+    };
+    //add error
+    let createCV = document.getElementById("create");
+    createCV.addEventListener("click", function(event) {
+        event.preventDefault();
+        lockLink(this);
+        addError();
+        setTimeout(() => closeError(), 3000);
+        setTimeout(() => unlockLink(this), 3000);
+    });
+    let auth = document.getElementById("auth");
+    auth.addEventListener("click", function(event) {
+        event.preventDefault();
+        lockLink(this);
+        addError();
+        setTimeout(() => closeError(), 3000);
+        setTimeout(() => unlockLink(this), 3000);
+    });
+    //view button bounce
+    let pulseView = document.getElementById("introduce");
+    pulseView.onmouseover = function(event) {
+        document.getElementById("viewCV").style =
+            "animation: bounce 1s linear infinite; text-shadow: 0px 0px 7px #000;";
+    };
+    pulseView.onmouseout = function(event) {
+        document.getElementById("viewCV").style = "animation: none;";
+    };
 });
-//#CF4917
